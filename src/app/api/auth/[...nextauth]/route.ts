137cf1d0523160526f6effec2f3117281e855149
Mokh Kumar Shah <mokshshah2020@gmail.com>
2026-01-17 20:13:14 +0530

    partially added authentication feature with bug

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,4 @@
+// import { handler } from "@/auth"
+
+
+// export const { GET, POST } = handlers
---
+++ src/app/lib/mongodb.ts
@@ -0,0 +1,25 @@
+import { MongoClient } from "mongodb";
+
+const uri = process.env.MONGODB_URI!;
+const options = {};
+
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
+if (!process.env.MONGODB_URI) {
+  throw new Error("Please add MONGODB_URI to .env");
+}
+
+if (process.env.NODE_ENV === "development") {
+  // prevent multiple connections in dev
+  if (!(global as any)._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    (global as any)._mongoClientPromise = client.connect();
+  }
+  clientPromise = (global as any)._mongoClientPromise;
+} else {
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
+}
+
+export default clientPromise;
