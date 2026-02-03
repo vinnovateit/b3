6b35a44a8e5cd3f13a6a3f1e9f215b43154361ef
Atiksh Chawla <atikshchawla@gmail.com>
2026-02-03 21:38:41 +0530

    fix: removed multiple common coponenets folder changed

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import WhoAreWe from "./components/WhoAreWe"; // Use ./ instead of ../ 
-import Hero from "../../components/Hero";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <>
+      <Navbar />
       <Hero/>
       <WhoAreWe />
     </>
