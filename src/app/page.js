0fbb9d520da88e28c2ee4e7554f0bb5f1f3b5fdf
Atiksh Chawla <atikshchawla@gmail.com>
2026-02-04 12:43:38 +0530

    fix: the extra scroll in hero + the button text shift while hover effect in button

--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,29 @@
 import CardsRow from "./components/CardsRow";
 import FAQ from "./components/FAQ";
 import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
+import Hero from "./components/Hero.jsx";
 import WhoAreWe from "./components/WhoAreWe";
 import Footer from "./components/footer/footer";
 
 export default function Home() {
-  return (
-    <main className="bg-[#040704]">
-      <Navbar />
-      <Hero />
-      <WhoAreWe />
-      {/* Timeline Section */}
-      <section className="relative overflow-visible">
-        <CardsRow />
-      </section>
+	return (
+		<main className="bg-[#040704]">
+			<Navbar />
+			<Hero />
+			<WhoAreWe />
+			{/* Timeline Section */}
+			<section className="relative overflow-visible">
+				<CardsRow />
+			</section>
 
-      {/* FAQ Section */}
-      <section 
-        className="relative overflow-hidden bg-[#040704] z-10" 
-        style={{ marginTop: '-10px' }} // Small overlap to hide the seam
-      >
-      <FAQ />
-      <Footer />
-      </section>
-    </main>
-  );
+			{/* FAQ Section */}
+			<section
+				className="relative overflow-hidden bg-[#040704] z-10"
+				style={{ marginTop: "-10px" }} // Small overlap to hide the seam
+			>
+				<FAQ />
+				<Footer />
+			</section>
+		</main>
+	);
 }
