f47c1049de07b34396ed6909d97003763561bb45
Lavanbarath B <71477639+lavan8t@users.noreply.github.com>
2026-02-05 10:46:09 +0530

    fix: merge Footer JSX and CSS module into single component

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
 import CardsRow from "./components/CardsRow";
 import FAQ from "./components/FAQ";
 import Navbar from "./components/Navbar";
-import Hero from "./components/Hero.jsx";
+import Hero from "./components/Hero";
 import WhoAreWe from "./components/WhoAreWe";
-import Footer from "./components/footer/footer";
+import Footer from "./components/Footer";
 
 export default function Home() {
 	return (
