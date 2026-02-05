e638e83159bc204b89c9ae14de9a85d033003eb5
AyushK0808 <theofficialayush.kumar@gmail.com>
2026-02-05 20:58:56 +0530

    fix: button component renamed

--- src/app/setup/team/page.jsx
+++ src/app/setup/team/page.jsx
@@ -1,6 +1,6 @@
 import SetupLayout from "@/app/components/SetupLayout";
 import SetupHeader from "@/app/components/SetupHeader";
-import Button from "@/app/components/Button";
+import CustomButton from "@/app/components/CustomButton";
 
 export default function TeamPage() {
 	return (
@@ -17,9 +17,9 @@ export default function TeamPage() {
 				style={{ height: "20vh" }}
 				className="flex items-start border-2 border-red-500"
 			>
-				<Button className="px-15 py-7 rounded-xl text-lg font-medium w-2/12">
+				<CustomButton className="px-15 py-7 rounded-xl text-lg font-medium w-2/12">
 					Next Step
-				</Button>
+				</CustomButton>
 			</div>
 		</SetupLayout>
 	);
